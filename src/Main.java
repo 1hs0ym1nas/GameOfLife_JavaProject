a41ca1ecdd10ef5cd9641365e34df0799f6c7125
min <min.ganzhimin@gmail.com>
2024-04-25 13:33:57 -0700

    add comments in view and view tests

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,12 @@ import GameOfLife.model.IModel;
 import GameOfLife.model.Model;
 import GameOfLife.view.View;
 
+/**
+ * Main class to start the application
+ */
 public class Main {
   public static void main(String[] args) {
+    // Model, Controller and View for the MVC model
     IModel model = Model.getModelInstance();
     AbstractController controller = Controller.getControllerInstance(model);
     View view = View.getViewInstance(controller);
